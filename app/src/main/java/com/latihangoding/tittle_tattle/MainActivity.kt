82193aa649803e37dc51d956283ade7a76bb9baf
Kevin Thiotanry <kevinthiotanry@pmberjaya.com>
2021-03-13 08:31:24 +0700

    initial commit

--- app/src/main/java/com/latihangoding/tittle_tattle/MainActivity.kt
+++ app/src/main/java/com/latihangoding/tittle_tattle/MainActivity.kt
@@ -1,31 +1,22 @@
 package com.latihangoding.tittle_tattle
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import android.view.inputmethod.EditorInfo
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.observe
 import com.latihangoding.tittle_tattle.databinding.ActivityMainBinding
 import com.latihangoding.tittle_tattle.vo.MyModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private val viewModel: MainViewModel by viewModels()
-
-//    override fun onSaveInstanceState(outState: Bundle) {
-//        super.onSaveInstanceState(outState)
-//        outState.putString(KEY, binding.tvMain.text.toString())
-//    }
-//
-//    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-//        super.onRestoreInstanceState(savedInstanceState)
-//        binding.tvMain.text = savedInstanceState?.getString(KEY)?: "Kosong"
-//    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,10 +25,6 @@ class MainActivity : AppCompatActivity() {
 
         initListener()
         initObserver()
-//        binding.buttonMain.setOnClickListener{
-//            binding.tvMain.text = binding.etMain.text
-//            binding.etMain.setText("")
-//        }
     }
 
     private fun initListener() {
@@ -52,15 +39,22 @@ class MainActivity : AppCompatActivity() {
             true
         }
 
+        lifecycleScope.launch {  }
+
 //        Ketika menekan button, maka akan pindah ke second activity
         binding.buttonMain.setOnClickListener {
-            val mIntent  = Intent(this, SecondActivity::class.java)
-            mIntent.putExtra(KEY, viewModel.myData.value) //passing data class ke intent menggunakan extra
+            val mIntent = Intent(this, SecondActivity::class.java)
+            mIntent.putExtra(
+                KEY,
+                viewModel.myData.value
+            ) //passing data class ke intent menggunakan extra
             startActivity(mIntent) //passing ke second activity
         }
     }
 
     private fun initObserver() {
+//  LiveData adalah kelas pemegang data yang dapat diamati. Tidak seperti observable biasa, LiveData peka terhadap siklus proses, artinya LiveData menghormati siklus proses komponen aplikasi lain, seperti aktivitas,
+//  fragmen, atau layanan. Kesadaran ini memastikan LiveData hanya memperbarui pengamat komponen aplikasi yang berada dalam status siklus proses aktif.
         viewModel.myData.observe(this) {
             binding.buttonMain.isEnabled = true
             binding.tvMain.text = "The message is: ${it.message}"
--- app/src/main/java/com/latihangoding/tittle_tattle/MainViewModel.kt
+++ app/src/main/java/com/latihangoding/tittle_tattle/MainViewModel.kt
@@ -7,6 +7,8 @@ import com.latihangoding.tittle_tattle.vo.MyModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
+//Kelas ViewModel dirancang untuk menyimpan dan mengelola data terkait UI dengan cara sadar siklus hidup.
+//Kelas ViewModel memungkinkan data bertahan dari perubahan konfigurasi seperti rotasi layar.
 @HiltViewModel
 class MainViewModel @Inject constructor() : ViewModel() {
     private val _myData = MutableLiveData<MyModel>()

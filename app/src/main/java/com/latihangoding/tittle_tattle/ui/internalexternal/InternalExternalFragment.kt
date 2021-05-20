2ef766b1dbb146719420c79368fd53cf410dcb32
yabelaurelius <yabelaurelius@gmail.com>
2021-05-20 16:33:40 +0700

    documentation

--- app/src/main/java/com/latihangoding/tittle_tattle/ui/internalexternal/InternalExternalFragment.kt
+++ app/src/main/java/com/latihangoding/tittle_tattle/ui/internalexternal/InternalExternalFragment.kt
@@ -26,6 +26,7 @@ class InternalExternalFragment : Fragment(), AdapterView.OnItemSelectedListener 
 
     var spinnerValue = listOf("New File")
 
+//    request permission untuk akses external storage
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<out String>,
@@ -44,6 +45,9 @@ class InternalExternalFragment : Fragment(), AdapterView.OnItemSelectedListener 
         }
     }
 
+//    ketika memilih salah satu file pada spinner, maka akan mengenable pengisian filename jika
+//    yang dipilih adalah item pada posisi pertama, sebaliknya jika file yang dipilih tidak pada posisi pertama
+//    maka filename akan didisable
     override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
         binding.etFilename.isEnabled = position == 0
         if (position != 0) {
@@ -61,6 +65,7 @@ class InternalExternalFragment : Fragment(), AdapterView.OnItemSelectedListener 
 
     }
 
+//    ketika create view akan menginflate fragment, dengan container. kemudian memulai listener, dan mendapatkan file internal pada folder yang ditentukan
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -76,6 +81,8 @@ class InternalExternalFragment : Fragment(), AdapterView.OnItemSelectedListener 
     }
 
     private fun initListener() {
+//        ketika button save ditekan maka akan menulis file internal jika switch dalam keadaan mati,
+//        sebaliknya akan menulis di file external jika switch dalam keadaan hidup
         binding.buttonSave.setOnClickListener {
             if (!binding.switchMain.isChecked)
                 writeFileInternal()
@@ -94,6 +101,8 @@ class InternalExternalFragment : Fragment(), AdapterView.OnItemSelectedListener 
             }
         }
 
+//        ketika switch terjadi perubahan. bila switch aktif maka akan mengambil external file
+//        bila switch mati maka akan mengambil internal file untuk dipaparkan di view
         binding.switchMain.setOnCheckedChangeListener { buttonView, isChecked ->
             if (!isExternalStorageReadable() && isChecked) {
                 buttonView.isChecked = false
@@ -109,6 +118,7 @@ class InternalExternalFragment : Fragment(), AdapterView.OnItemSelectedListener 
         }
     }
 
+//    mengambil semua data dari internal file dengan extension ".txt", namun list pertama akan diisi dengan "New File"
     private fun getInternalFiles() {
         val tmp = mutableListOf("New File")
         for (file in activity?.fileList()?.toList() ?: emptyList()) {
@@ -119,6 +129,8 @@ class InternalExternalFragment : Fragment(), AdapterView.OnItemSelectedListener 
         initSpinner()
     }
 
+//    menulis data ke filter internal, kemudian menyimpan file dengan filename permintaan dengan extension ".txt"
+//    kemudian menuliskan data yang di view note ke file tersebut
     private fun writeFileInternal() {
         var filename = binding.etFilename.text.toString()
         if (filename.contains(".txt")) {
@@ -133,13 +145,15 @@ class InternalExternalFragment : Fragment(), AdapterView.OnItemSelectedListener 
         Toast.makeText(requireContext(), "File Save", Toast.LENGTH_SHORT).show()
     }
 
+//    membaca file internal sesuai dengan posisi yang diinginkan
     private fun readFileInternal(position: Int) {
         val filename = spinnerValue[position]
+//    set view_filename dengan filename tersebut
         binding.etFilename.setText(filename)
-
         binding.etNote.text.clear()
 
         try {
+//            mencoba membuka file dengan filename tersebut
             activity?.openFileInput(filename)?.apply {
                 var mtext = ""
                 bufferedReader().useLines {
@@ -147,8 +161,10 @@ class InternalExternalFragment : Fragment(), AdapterView.OnItemSelectedListener 
                         mtext += "$text\n"
                     }
                 }
+//                set view_note dengan isi file
                 binding.etNote.setText(mtext.trim())
             }
+//            apabila gagal maka file not found atau file can't be read sesuai exception
         } catch (e: FileNotFoundException) {
             binding.etNote.setText("File Not Found")
         } catch (e: IOException) {
@@ -156,6 +172,7 @@ class InternalExternalFragment : Fragment(), AdapterView.OnItemSelectedListener 
         }
     }
 
+//    //    mengambil semua data dari internal file dengan extension ".txt", kemudian disimpan ke directory documents
     private fun getExternalFiles() {
         val tmp = mutableListOf("New File")
         for (file in activity?.getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS)?.listFiles()
@@ -167,22 +184,27 @@ class InternalExternalFragment : Fragment(), AdapterView.OnItemSelectedListener 
         initSpinner()
     }
 
+//    menulis data ke file external
     private fun writeFileExternal() {
+//    apabila external storage tidak bisa diakses maka return
         if (!isExternalStorageReadable()) return
         var filename = binding.etFilename.text.toString()
         if (filename.contains(".txt")) {
             filename = filename.substring(0, filename.indexOf(".txt"))
         }
         val myDir = File(activity?.getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS)?.toURI())
+//    apabila directory_documents tidak ada, maka akan create folder tersebut
         if (!myDir.exists()) {
             myDir.mkdir()
         }
+//    kemudian menulis data ke folder tersebut sesuai dengan filename dan extension ".txt"
         File(myDir, "$filename.txt").apply {
             writeText(binding.etNote.text.toString().trim())
             Toast.makeText(requireContext(), "File Save", Toast.LENGTH_SHORT).show()
         }
     }
 
+//    membaca file external pada posisi di spinner
     private fun readFileExternal(position: Int) {
         if (!isExternalStorageReadable()) return
         val filename = spinnerValue[position]
@@ -195,6 +217,7 @@ class InternalExternalFragment : Fragment(), AdapterView.OnItemSelectedListener 
         binding.etNote.setText(readFile.trim())
     }
 
+//    cek apaah external storage dapat diakses / dibaca
     private fun isExternalStorageReadable(): Boolean {
         if (ContextCompat.checkSelfPermission(
                 requireContext(),

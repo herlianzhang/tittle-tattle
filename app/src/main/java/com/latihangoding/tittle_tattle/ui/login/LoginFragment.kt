abcdaa931566bf9fe511fed812f00cb85162d3c3
herlianzhang <herlianzhang370@gmail.com>
2021-06-27 18:33:41 +0700

    update

--- app/src/main/java/com/latihangoding/tittle_tattle/ui/login/LoginFragment.kt
+++ app/src/main/java/com/latihangoding/tittle_tattle/ui/login/LoginFragment.kt
@@ -18,6 +18,9 @@ import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import com.latihangoding.tittle_tattle.R
@@ -39,12 +42,14 @@ class LoginFragment : Fragment() {
 
     private lateinit var adsPreferences: AdsPreferences
 
+    private var premiumUsers = listOf<String>() // mencatat uid premium member
+
     private val loginResult = registerForActivityResult(StartActivityForResult()) { result ->
         if (result.resultCode == Activity.RESULT_OK) {
             val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
             try {
                 val account = task.getResult(ApiException::class.java)!!
-                firebaseAuthWithGoogle(account.idToken!!)
+                firebaseAuthWithGoogle(account.idToken!!) // setelah login berhasil masuk ke fungsi untuk meyimpan data user.
             } catch (e: ApiException) {
                 Toast.makeText(requireContext(), "Google sign in failed", Toast.LENGTH_SHORT).show()
                 Timber.e("Masuk fail $e")
@@ -59,15 +64,16 @@ class LoginFragment : Fragment() {
         binding = FragmentLoginBinding.inflate(inflater, container, false)
 
         adsPreferences = AdsPreferences(requireContext())
-        binding.switchAds.isChecked = adsPreferences.isMuteAds // set status terakhir ke switch
 
         initObject()
         initListener()
+        getPremiumUsers()
 
         return binding.root
     }
 
     private fun initObject() {
+        // init googlesigninclient
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestIdToken(getString(R.string.default_web_client_id))
             .requestEmail()
@@ -79,34 +85,47 @@ class LoginFragment : Fragment() {
     }
 
     private fun initListener() {
-        binding.buttonLogin.setOnClickListener {
+        binding.buttonLogin.setOnClickListener { // perform login saat button ini ditekan
             googleSignInClient.signOut()
             val signInIntent = googleSignInClient.signInIntent
             loginResult.launch(signInIntent)
         }
+    }
 
-        binding.switchAds.setOnCheckedChangeListener { _, isChecked ->
-            adsPreferences.isMuteAds = isChecked // set status isMute jika checked berubah
-        }
+    private fun getPremiumUsers() {
+        Firebase.database.getReference(FirebaseConfiguration.PREMIUM).addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) { // fetch uid user premium
+                val tmp = mutableListOf<String>()
+                for (d in snapshot.children) {
+                    tmp.add(d.getValue(String::class.java) ?: "")
+                }
+                premiumUsers = tmp // memasukan data uid user premium ke wadah yang tadi sudah dibuat
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+
+        })
     }
 
     private fun firebaseAuthWithGoogle(idToken: String) {
         val credential = GoogleAuthProvider.getCredential(idToken, null)
-        auth.signInWithCredential(credential)
+        auth.signInWithCredential(credential) // melakukan signin with gogole
             .addOnCompleteListener(requireActivity()) { task ->
                 if (task.isSuccessful) {
                     val userId = task.result?.user?.uid ?: return@addOnCompleteListener
                     val email = task.result?.user?.email ?: return@addOnCompleteListener
                     val photoUrl = task.result?.user?.photoUrl ?: return@addOnCompleteListener
                     val displayName = task.result?.user?.displayName ?: return@addOnCompleteListener
-
-                    Timber.d("Masuk email $email, photoUrl $photoUrl, displayName: $displayName")
                     binding.pbLoading.isVisible = true
                     val data = User(displayName, email, photoUrl.toString())
-                    Firebase.database.getReference(FirebaseConfiguration.USER).child(userId)
+                    Firebase.database.getReference(FirebaseConfiguration.USER).child(userId) // menyimpan data user setelah login
                         .setValue(data).addOnCompleteListener {
                             binding.pbLoading.isVisible = false
                             if (it.isSuccessful) {
+                                adsPreferences.isMuteAds = premiumUsers.contains(userId) // jika user merupakan user premium
+                                                                                        // maka iklan ditiadakan
                                 findNavController().navigate(R.id.action_loginFragment_to_roomChatFragment)
                             } else {
                                 Toast.makeText(requireContext(), "Login Fail", Toast.LENGTH_SHORT)
--- app/src/main/java/com/latihangoding/tittle_tattle/utils/FirebaseConfiguration.kt
+++ app/src/main/java/com/latihangoding/tittle_tattle/utils/FirebaseConfiguration.kt
@@ -3,4 +3,5 @@ package com.latihangoding.tittle_tattle.utils
 object FirebaseConfiguration {
     const val USER = "users"
     const val CHAT = "chat"
+    const val PREMIUM = "premium_users"
 }
